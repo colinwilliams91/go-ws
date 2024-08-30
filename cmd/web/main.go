87fc2860daaec40545aaf50194e29ef78cbb60b9
Colin Williams <colin.williams.dev@gmail.com>
2024-08-29 23:36:53 -0500

    :sparkles:: start web server in main, import routes to leverage our mux http.Handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 // placeholder
 
 func main() {
+	var mux http.Handler = routes()
 
+	log.Println("Starting wen server on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
 }
