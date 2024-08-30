d2747191fa056e976d730c2593583fa6bddf0554
Colin Williams <colin.williams.dev@gmail.com>
2024-08-29 23:41:21 -0500

    :nail_care:: html template h1 and web server terminal typo

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	var mux http.Handler = routes()
 
-	log.Println("Starting wen server on port 8080")
+	log.Println("Starting web server on port 8080")
 
 	_ = http.ListenAndServe(":8080", mux)
 }
