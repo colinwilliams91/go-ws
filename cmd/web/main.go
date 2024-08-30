c012631ab57595f5a1d21af2a7baf256104b1165
Colin Williams <colin.williams.dev@gmail.com>
2024-08-29 23:20:47 -0500

    :sparkles:: implement jet templating, renderPage func, logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,7 @@
 package main
 
+// placeholder
+
 func main() {
 
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,34 @@
 package handlers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
+)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
 
 func Home(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func renderPage(w http.ResponseWriter, template string, data jet.VarMap) error {
+	view, err := views.GetTemplate(template)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
 }
