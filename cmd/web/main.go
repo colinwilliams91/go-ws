bc4f5bb04e75048fd5465c701ad1620b915b5160
Colin Williams <colin.williams.dev@gmail.com>
2024-09-02 22:05:34 -0500

    :sparkles:: call goroutine ListenToWs in main.go server start

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/colinwilliams91/go-ws.git/internal/handlers"
 )
 
 // placeholder
@@ -10,7 +12,11 @@ import (
 func main() {
 	var mux http.Handler = routes()
 
-	log.Println("Starting web server on port 8080")
+	log.Println("Starting channel listener...")
+
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on port 8080...")
 
 	_ = http.ListenAndServe(":8080", mux)
 }
